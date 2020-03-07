a299db5cd2c701d1576443631509a034425a57da
taehyunlim <taehyun.lim@gmail.com>
2020-03-06 16:50:20 -0800

    keeping couponLineItems when resetting cart to avoid cart data discrepancy when shipping addr is updated

--- cartridges/int_affirm_sfra/cartridge/controllers/Affirm.js
+++ cartridges/int_affirm_sfra/cartridge/controllers/Affirm.js
@@ -315,9 +315,6 @@ server.use('CreateOrder', function (req, res, next) {
         basket.getAllProductLineItems().toArray().forEach(function (item) {
             basket.removeProductLineItem(item);
         });
-        basket.getCouponLineItems().toArray().forEach(function (item) {
-            basket.removeCouponLineItem(item);
-        });
     
         Transaction.wrap(function () {
             products.forEach(function (prod) {
